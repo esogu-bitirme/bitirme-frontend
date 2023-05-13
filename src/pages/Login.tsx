821d46c1e79a9b35be3d437ef1974221b1f6df89
Bora Soylu <mail@borasoylu.com>
2023-05-13 17:26:25 +0300

    login page started

---
+++ src/pages/Login.tsx
@@ -0,0 +1,89 @@
+import loginImage from './login-page-image.webp';
+
+export const Login = () => {
+  return (
+    <div className="flex w-full">
+      <div className="flex items-center justify-center w-1/2">
+        <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+          <div className="self-center mb-2 text-xl text-gray-600 sm:text-2xl dark:text-white font-bold">
+            Giriş Yap
+          </div>
+          <div className="mt-8">
+            <form action="#" autoComplete="off">
+              <div className="flex flex-col mb-2">
+                <div className="flex relative ">
+                  <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow text-sm">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      width="15"
+                      height="15"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+                      />
+                    </svg>
+                  </span>
+                  <input
+                    type="text"
+                    id="sign-in-email"
+                    className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    placeholder="T.C. Kimlik No"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col mb-6">
+                <div className="flex relative ">
+                  <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                    <svg
+                      width="15"
+                      height="15"
+                      className="w-4 h-4"
+                      fill="currentColor"
+                      viewBox="0 0 1792 1792"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
+                    </svg>
+                  </span>
+                  <input
+                    type="password"
+                    id="sign-in-email"
+                    className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    placeholder="Şifre"
+                  />
+                </div>
+              </div>
+              <div className="flex w-full">
+                <button
+                  type="submit"
+                  className="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="flex items-center justify-center mt-6">
+            <a
+              href="#"
+              target="_blank"
+              className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
+            >
+              <span className="ml-2">You don&#x27;t have an account?</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <img src="./login-page-image.webp" alt="Elinde meme kanseri tutan birinin logosu" />
+      </div>
+    </div>
+  );
+};
